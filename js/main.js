7b0d0668a0f051d2f60c6c17a1b17a7b907e9c30
Luca <mylince@gmail.com>
2020-03-04 14:25:11 +0100

    Aggiunto responsive cell, apertura hamburger (lista sms) e chiusura.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,24 @@ $('.chat').mouseleave(function(){
 });
 
 
+//Se clicco sull'hamburger si apre la schermata dei contatti
+$('.icon-right i.fas.fa-bars').click(function(){
+    var right = $('.right.view');
+    var left = $('.left');
+
+    right.removeClass('view');
+    left.addClass('view');
+ });
+
+
+// Se clicco sull'icon X, si chiude la schermata dei contatti e ritorna di conseguenza su quella della chat
+$('.icon-left i.fas.fa-times').click(function(){
+    var left = $('.left');
+
+    left.removeClass('view');
+});
+
+
 //Cambio icona micrfono con testo quando muovo il mouse dentro all'imput per inseirire il testo.
 $('.bottom-right input').mouseenter(function(){
     var imgMic = $('.bottom-right i.fas.fa-microphone.active');
