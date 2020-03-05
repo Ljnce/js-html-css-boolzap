e0e7e57bbd31649e2e8a2f49556d0937595c1bd5
Luca <mylince@gmail.com>
2020-03-05 17:10:16 +0100

    Add

--- js/main.js
+++ js/main.js
@@ -48,10 +48,69 @@ $('.bottom-right input').mouseenter(function(){
     imgText.addClass('active');
 })
 
-$('.bottom-right input').mouseleave(function(){
-    var imgText = $('.bottom-right i.fas.fa-comment-dots.active');
+$('i.fas.fa-comment-dots').click(function(){
+     var imgText = $('.bottom-right i.fas.fa-comment-dots.active');
     var imgMic = $('.bottom-right i.fas.fa-microphone');
 
-    imgText.removeClass('active');
-    imgMic.addClass('active');
+     imgText.removeClass('active');
+     imgMic.addClass('active');
+ })
+
+
+//Input che mi da il messaggio visualizzato a destra
+$('i.fas.fa-comment-dots').click(function(){
+    var nomeInput = $('#nome').val(); //invio messaggio dall'input
+    $('#nome').val('');//resetto quello che c'e dentro
+    // $('.testo-messaggio').text(nomeInput);// me lo scrive dove c'è nome utente
+    // $('.orario').text(time);// me lo scrive dove c'è nome utente
+
+    // $('.chat-dx').append(nomeInput) // scrive e non sovrascrive
+    var messaggio = $('.template .messaggio').clone();//clonato tmeplate  emesso sotto lista nomi sotto
+    messaggio.children('.testo-messaggio').text(nomeInput);
+    messaggio.children('.orario').text(time);
+    $('.center-right').append(messaggio); //in lista nomi metto messaggio
 })
+
+
+//Imposta l'ora corrente da far apparire sui messaggi
+var clock = new Date();
+
+var time = clock.getHours() + ':' + clock.getMinutes();
+
+
+//Risposta a sinistra sui messaggi
+$('i.fas.fa-comment-dots').click(function(){
+    var nomeInput = $('#nome').val(); //invio messaggio dall'input
+    $('#nome').val('');//resetto quello che c'e dentro
+    // $('.orario2').text(time);// me lo scrive dove c'è nome utente
+
+    // $('.lista-nomi').append(nomeInput) // scrive e non sovrascrive
+    var messaggio2 = $('.template2 .messaggio2').clone();//clonato tmeplate  emesso sotto lista nomi sotto
+    messaggio2.children('.testo-messaggio2').text();
+    messaggio2.children('.orario2').text(time);
+    $('.center-right').append(messaggio2); //in lista nomi metto messaggio
+});
+
+//Le spunte diventano blu dopo 2 secondi
+$('i.fas.fa-comment-dots').click(function(){
+    setTimeout(function(){
+    $('i.fas.fa-check-double').addClass('color-icon');
+    }, 2000);
+});
+
+
+//Trovo i nomeInput
+$('#search').keyup(function(event){ //evento
+    var ricercaContatto = $(this).val().toLowerCase();// prendo il valore di questo
+    // console.log(ricercaContatto);//MI TROVA TUTE LE LETTERE
+    $('.chat').each(function(){//se nella lista contatti, è preesente il carattere digiato, visulazziarlo
+        // console.log($(this).text());
+        var name = $(this).find('p').text().toLowerCase();
+        if(name.includes(ricercaContatto)){ //se il nome del list item  ha al suo interno uno dei caratteri digitati, visualizzalo
+            $(this).show();
+        } else{ //altrimenti dispaly none, non visualizzarlo
+            $(this).hide();
+            // $('.chat').not(this).hide();
+        }
+    });
+});
